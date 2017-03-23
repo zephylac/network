c1e65f522a5cf61ad022a9238c86f1e76dbfbf64
zephylac <spi2163@ic2s122-10.univ-lemans.fr>
2017-03-23 11:59:02 +0100

    filetransfer

---
+++ file/client.c
@@ -0,0 +1,79 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define SERVER_PORT 1500
+#define MAX_MSG 100
+
+int main(int argc, char *argv[]){
+	int sd, rc, i = 0;
+	char * car;
+	FILE * file1;
+	struct sockaddr_in localAddr, servAddr;
+	struct hostent *h;
+
+	if(argc != 3){
+		printf("usage: %s <server> <filename>\n",argv[0]);
+		exit(1);
+	}
+	h = gethostbyname(argv[1]);
+	
+	if(h == NULL){
+		printf("%s : unknown host '%s'\n",argv[0],argv[1]);
+	}
+	
+	servAddr.sin_family = h -> h_addrtype;
+	memcpy((char *) &servAddr.sin_addr.s_addr, h -> h_addr_list[0], h-> h_length);
+	servAddr.sin_port = htons(SERVER_PORT);
+
+	sd = socket(AF_INET, SOCK_STREAM, 0);
+	if(sd < 0){
+		perror("cannot open socket");
+		exit(1);
+	}
+
+	localAddr.sin_family = AF_INET;
+	localAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	localAddr.sin_port = htons(0);
+
+	rc = bind(sd, (struct sockaddr *) &localAddr, sizeof(localAddr));
+	if(rc < 0){
+		printf("%s : cannot bind port TCP %u\n", argv[0], SERVER_PORT);
+		perror("error");
+		exit(1);
+	}
+
+	rc = bind(sd, (struct sockaddr *) &servAddr, sizeof(servAddr));
+	if(rc < 0){
+		perror("cannot connect");
+		exit(1);
+	}
+	rc = send(sd, argv[2], strlen(argv[2]) + 1, 0);
+	if(rc < 0){
+		perror("cannot send data");
+		close(sd);
+		exit(1);
+	}
+	printf("%s: namefile sent (%s)\n", argv[0], argv[2]);
+
+	file1 = fopen(argv[2],"r");
+	while(!feof(file1)){
+		fscanf(file1,"%c",car);
+		rc = send(sd,(void*) car, sizeof(char), 0);
+		if(rc < 0){
+			perror("cannot send data");
+			close(sd);
+			exit(1);
+		}
+		printf("%s: data%u sent (%s)\n", argv[0],i-1, car);
+		i++;
+	}
+	return 0;
+}
+	
